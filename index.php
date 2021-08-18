811c9f59da44e46d478077ab992508a3734cf5ba
Wasinee2516 <71056676+Wasinee2516@users.noreply.github.com>
2021-08-18 16:43:14 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,6 @@
-<h1>Wasinee Na Ranong</h1>
+<body>
+<br><h1>Wasinee Na Ranong</h1></br>
+<img src="https://thingspeak.com/channels/1458415/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" width="500" height="333">
+< src="https://thingspeak.com/channels/1458415/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15">
+
+  </body>
